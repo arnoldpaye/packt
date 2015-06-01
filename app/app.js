4389cba00dfe37bb2e3ea8e68513bb40f4ccaa1e
arnoldpaye <arnoldpayet@gmail.com>
2015-06-01 15:16:09 -0400

    splash screen

--- app/app.js
+++ app/app.js
@@ -14,16 +14,46 @@
 //@require @packageOverrides
 
 Ext.application({
+    
     name: 'Packt',
-
+    
+    requires: [
+        'Packt.view.Login'
+    ],
+    
     views: [
-        'Main',
-        'Viewport'
+        'Login'
     ],
-
-    controllers: [
-        'Main'
-    ],
-
-    autoCreateViewport: true
+    
+    init: function() {
+        
+        splashscreen = Ext.getBody().mask('Loading application', 'splashscreen');
+        
+        Ext.DomHelper.insertFirst(Ext.query('.x-mask-msg')[0], {
+            cls: 'x-splash-icon'
+        });
+        
+    },
+    
+    launch: function() {
+        var task = new Ext.util.DelayedTask(function() {
+            
+            splashscreen.fadeOut({
+                duration: 1000,
+                remove: true
+            });
+            
+            splashscreen.next().fadeOut({
+                duration: 1000,
+                remove: true,
+                listeners: {
+                    afteranimate: function(el, startTime, eOpts) {
+                        Ext.widget('login');
+                    }
+                }
+            });
+        });
+        
+        task.delay(2000);
+    }
 });
